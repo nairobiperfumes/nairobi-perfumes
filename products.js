a9aa061cfefedb9c1f43a70fc6fd98f6cdf6ef9d
Nairobi Perfumes <nairobiperfumes254@gmail.com>
2026-05-06 10:42:26 +0300

    real products added

---
+++ products.js
@@ -0,0 +1,86 @@
+const products = [
+  {
+    name: "Badee Al Oud Sublime",
+    price: 3800,
+    oldPrice: 4200,
+    category: "Arabic",
+    image: "images/badee-al-oud-sublime.jpg"
+  },
+  {
+    name: "Yara",
+    price: 2800,
+    oldPrice: 3500,
+    category: "Arabic",
+    image: "images/yara.jpg"
+  },
+  {
+    name: "Yara Candy",
+    price: 2800,
+    oldPrice: 3500,
+    category: "Arabic",
+    image: "images/yara-candy.jpg"
+  },
+  {
+    name: "Eclaire Perfume",
+    price: 3800,
+    oldPrice: 4500,
+    category: "Arabic",
+    image: "images/eclaire.jpg"
+  },
+  {
+    name: "Her Confession",
+    price: 4200,
+    oldPrice: 4500,
+    category: "Arabic",
+    image: "images/her-confession.jpg"
+  },
+  {
+    name: "His Confession",
+    price: 3900,
+    oldPrice: 4500,
+    category: "Arabic",
+    image: "images/his-confession.jpg"
+  },
+  {
+    name: "Ana Abiyedh Rouge",
+    price: 3000,
+    oldPrice: 3500,
+    category: "Arabic",
+    image: "images/ana-rouge.jpg"
+  },
+  {
+    name: "Ana Abiyedh Coral",
+    price: 2800,
+    oldPrice: 3500,
+    category: "Arabic",
+    image: "images/ana-coral.jpg"
+  },
+  {
+    name: "Khamrah",
+    price: 3800,
+    oldPrice: 4200,
+    category: "Arabic",
+    image: "images/khamrah.jpg"
+  },
+  {
+    name: "Asad",
+    price: 2700,
+    oldPrice: 3500,
+    category: "Arabic",
+    image: "images/asad.jpg"
+  },
+  {
+    name: "Nebras",
+    price: 3500,
+    oldPrice: 4000,
+    category: "Arabic",
+    image: "images/nebras.jpg"
+  },
+  {
+    name: "Yara Moi",
+    price: 2800,
+    oldPrice: 3500,
+    category: "Arabic",
+    image: "images/yara-moi.jpg"
+  }
+];
